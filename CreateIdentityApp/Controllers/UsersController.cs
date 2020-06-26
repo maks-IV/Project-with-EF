1a6aed63f1c8c6236d5a5836625a554e888a4bd8
Maks Ivasenko <ivasenko182@gmail.com>
2020-06-26 23:56:17 +0300

    Add administration panel

---
+++ CreateIdentityApp/Controllers/UsersController.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CreateIdentityApp.Models;
+using CreateIdentityApp.ViewModels;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CreateIdentityApp.Controllers
+{
+    public class UsersController : Controller
+    {
+        UserManager<User> _userManager;
+        public UsersController(UserManager<User> userManager)
+        {
+            _userManager = userManager;
+        }
+        public IActionResult Index() => View(_userManager.Users.ToList());
+        public IActionResult Create() => View();
+
+        [HttpPost]
+        public async Task<IActionResult> Create(CreateUserViewModel model)
+        {
+            if(ModelState.IsValid)
+            {
+                User user = new User { UserName = model.Email, Email = model.Email, YearBirth = model.YearBirth };
+                var result = await _userManager.CreateAsync(user,model.Password);
+                if(result.Succeeded)
+                {
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    foreach(var error in result.Errors)
+                    {
+                        ModelState.AddModelError(String.Empty, error.Description);
+                    }
+                }
+            }
+            return View(model);
+        }
+
+        [HttpGet] //?
+        public async Task<IActionResult> Edit(string id)
+        {
+            User user = await _userManager.FindByIdAsync(id);
+            if(user == null)
+            {
+                return NotFound();
+            }
+            EditUserViewModel model = new EditUserViewModel { Id = user.Id, Email = user.Email, YearBirth = user.YearBirth };
+            return View(model);
+        }
+
+        [HttpPost] 
+        public async Task<IActionResult> Edit(EditUserViewModel model)
+        {
+            if(ModelState.IsValid)
+            {
+                User user = await _userManager.FindByIdAsync(model.Id);
+                if(user != null)
+                {
+                    user.Email = model.Email;
+                    user.UserName = model.Email;
+                    user.YearBirth = model.YearBirth;
+
+                    var result = await _userManager.UpdateAsync(user);
+                    if(result.Succeeded)
+                    {
+                        return RedirectToAction("Index");
+                    }
+                    else
+                    {
+                        foreach (var error in result.Errors)
+                        {
+                            ModelState.AddModelError(string.Empty, error.Description);
+                        }
+                    }
+                }
+            }
+            return View(model);
+        }
+
+        [HttpPost] 
+        public async Task<IActionResult> Delete(string id)
+        {
+            User user = await _userManager.FindByIdAsync(id);
+            if(user != null)
+            {
+                IdentityResult result = await _userManager.DeleteAsync(user);
+            }
+            return RedirectToAction("Index");
+        }
+    }
+}
