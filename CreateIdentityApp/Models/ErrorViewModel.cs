74b3965e7c600a7fb3c3c2c2ea95664f488607bc
Maks Ivasenko <ivasenko182@gmail.com>
2020-06-26 23:04:24 +0300

    Add project files.

---
+++ CreateIdentityApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CreateIdentityApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
